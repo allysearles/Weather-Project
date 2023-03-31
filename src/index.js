e61eb754fe114d33ba0058855a35cb3e49bc8e62
allysearles <alyssa.searles001@mymdc.net>
2023-03-31 14:46:02 -0400

    Worked On: Adding functionality to the forecast in JS.

--- src/index.js
+++ src/index.js
@@ -133,11 +133,12 @@ function formatDate(timestamp) {
 	let day = days[date.getDay()];
 	return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+function displayForecast(response) {
+	console.log(response.data.daily);
 	let forecastElement = document.querySelector("#forecast");
 
 	let forecastHTML = `<div class="row">`;
-	let days = ["MON", "TUE", "WED", "THU", "FRI", "SAT"];
+	let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
 	days.forEach(function (day) {
 		forecastHTML =
 			forecastHTML +
@@ -162,7 +163,14 @@ function displayForecast() {
 	forecastElement.innerHTML = forecastHTML;
 }
 
-function showTemperature(response) {
+function getForecast(coordinates) {
+	console.log(coordinates);
+	let apiKey = "f4fa6962f95e93ddf91bbb3cb979b7b4";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+	axios.get(apiUrl).then(displayForecast);
+}
+
+function displayTemperature(response) {
 	let localtemperatureElement = document.querySelector("#localtemperature");
 	let localcityElement = document.querySelector("#localcity");
 	let descriptionElement = document.querySelector("#description");
@@ -184,12 +192,14 @@ function showTemperature(response) {
 		`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 	);
 	iconElement.setAttribute("alt", response.data.weather[0].description);
+
+	getForecast(response.data.coord);
 }
 
 function search(city) {
 	let apiKey = "f4fa6962f95e93ddf91bbb3cb979b7b4";
 	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-	axios.get(apiUrl).then(showTemperature);
+	axios.get(apiUrl).then(displayTemperature);
 }
 
 function handlesubmit(event) {
@@ -227,4 +237,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-displayForecast();
